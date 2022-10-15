6f48c020e896136b4e0db7397c0df4c96fbe50c9
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-15 12:13:25 +0530

    Implement Redux concept using counter example.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,29 @@
 import classes from './Counter.module.css';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 
 const Counter = () => {
+
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({type : "increment"})
+  }
+
+  const decrement = () => {
+    dispatch({type : "decrement"})
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
