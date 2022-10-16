7d88a0e5abdc0ac286fc1a68bf1a825bcb5d8d5c
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-16 14:07:40 +0530

    Another way to create redux store (commented -> only for one slice).

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
+// import { createStore, createSlice } from "@reduxjs/toolkit"; // becuase redux package also included inside @reduxjs/toolkit.
 
 const initialState = { counter : 0, showCounter : true };
 
@@ -21,6 +22,7 @@ const counterSlice = createSlice({
     }
 })
 
+// const store = createStore(counterSlice.reducer); // this is work for only one Slice.
 const store = configureStore({ reducer : counterSlice.reducer });
 
 export const counterActions = counterSlice.actions;
